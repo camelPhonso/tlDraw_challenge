f3f3fb95c944b0d39ac19e6852522fb2153e2444
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-04-01 20:42:33 +0100

    added bin to delete pasted stickers
    closes #2 users can now drag any sticker from the whiteboard onto a bin in order to delete it. This feature completes the user story selected for this challenge.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // global variables //////////////////////
 /////////////////////////////////////////
 const board = document.getElementById("board");
+const picker = document.getElementById("picker");
 
 // templates //////////////////////////
 //////////////////////////////////////
@@ -24,30 +25,27 @@ let existsCarbonCopy = () => {
   return document.getElementById("copied-sticker") !== null ? true : false;
 };
 
-function findCarbonCopy() {
-  return document.getElementById("copied-sticker");
-}
-
 // copy a sticker /////////////////////////////////////
 //////////////////////////////////////////////////////
 function copy(sticker) {
   let newClass = findModifierClass(sticker);
 
-  if (!existsCarbonCopy()) return createCarbonCopy(newClass);
-  swapClassOnCopy(newClass);
+  if (existsCarbonCopy()) return swapClassOnCopy(newClass);
+  createCarbonCopy(newClass);
 }
 
 // identify the correct sticker to copy
 function findModifierClass(sticker) {
-  let regexStickerClasses = new RegExp(/(sticker--)/);
+  let regexModifierClass = new RegExp(/(sticker--)/);
   let classListArray = Array.from(sticker.classList);
-  let filteredList = classListArray.filter((i) => regexStickerClasses.test(i));
-  return filteredList[0];
+  let filteredList = classListArray.filter((i) => regexModifierClass.test(i));
+
+  return filteredList[0]; // only one modifier class applied to a sticker at any time
 }
 
 function swapClassOnCopy(newClass) {
-  // always called after a successfull existsCarbonCopy()
-  let carbonCopy = findCarbonCopy();
+  // ONLY ever called after a successfull existsCarbonCopy()
+  let carbonCopy = document.getElementById("copied-sticker");
   let oldClass = findModifierClass(carbonCopy);
 
   carbonCopy.classList.remove(oldClass);
@@ -60,7 +58,7 @@ function swapClassOnCopy(newClass) {
 function dragCarbonCopy(event) {
   if (!existsCarbonCopy()) return;
 
-  let carbonCopy = findCarbonCopy();
+  let carbonCopy = document.getElementById("copied-sticker");
   let mouseX = event.clientX;
   let mouseY = event.clientY;
 
@@ -72,15 +70,18 @@ function dragCarbonCopy(event) {
 // paste a copy onto the whiteboard /////////////////
 ////////////////////////////////////////////////////
 function dropCarbonCopy() {
-  let carbonCopy = findCarbonCopy();
+  let carbonCopy = document.getElementById("copied-sticker");
   carbonCopy.classList.add("pasted");
   carbonCopy.setAttribute("draggable", "true");
+
+  addListeners(carbonCopy);
+
   carbonCopy.removeAttribute("id", "copied-sticker");
 }
 
 function pasteCarbonCopy() {
   if (!existsCarbonCopy()) return;
-  let carbonCopy = findCarbonCopy();
+  let carbonCopy = document.getElementById("copied-sticker");
   let newClass = findModifierClass(carbonCopy);
 
   dropCarbonCopy();
@@ -92,12 +93,16 @@ function pasteCarbonCopy() {
 function deleteCarbonCopy() {
   if (!existsCarbonCopy()) return;
 
-  findCarbonCopy().remove();
+  document.getElementById("copied-sticker").remove();
+}
+
+function deleteUsedSticker() {
+  document.getElementById("used-sticker").remove();
 }
 
 // event listeners /////////////////////////
 ///////////////////////////////////////////
-// copy sticker
+// make a carbon copy of a sticker in the picker tab
 let stickers = document.querySelectorAll(".sticker");
 stickers.forEach((sticker) => {
   sticker.addEventListener("click", () => copy(sticker));
@@ -116,27 +121,39 @@ document.addEventListener("keydown", (e) => {
 
 ///// drag and drop used stickers ///////////////////
 ////////////////////////////////////////////////////
+function addListeners(element) {
+  // applied everytime a sticker is pasted
+  element.addEventListener("dragenter", (e) => {
+    let sticker = e.target;
+
+    sticker.style.opacity = "0%";
+    sticker.setAttribute("id", "used-sticker");
+  });
+
+  element.addEventListener("dragleave", (e) => {
+    let sticker = document.getElementById("used-sticker");
+
+    sticker.style.position = "absolute";
+    sticker.style.top = `${e.clientY}px`;
+    sticker.style.left = `${e.clientX}px`;
+  });
+}
+
 board.addEventListener("dragover", (e) => {
   e.preventDefault();
-});
-
-board.addEventListener("dragenter", (e) => {
-  let sticker = e.target;
-
-  sticker.style.opacity = "0%";
-  sticker.setAttribute("id", "used-sticker");
-});
-
-board.addEventListener("dragleave", (e) => {
-  let sticker = e.target;
-
-  sticker.style.position = "absolute";
-  sticker.style.top = `${e.clientY - 30}px`;
-  sticker.style.left = `${e.clientX - 30}px`;
+  let picker = document.getElementById("picker");
+  picker.classList.add("trash-bin");
 });
 
 board.addEventListener("dragend", (e) => {
   let sticker = document.getElementById("used-sticker");
   sticker.style.opacity = "100%";
   sticker.removeAttribute("id", "used-sticker");
+
+  let trashBin = document.querySelector(".trash-bin");
+  trashBin.classList.remove("trash-bin");
+});
+
+picker.addEventListener("dragover", (e) => {
+  e.preventDefault(); // to prevent dragging animation when deleting a used sticker
 });
